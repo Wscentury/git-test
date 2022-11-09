8c13551ed55dfc1e763c678cae7bc819da1d2599
century <2351991750@qq.com>
2022-11-09 12:19:27 +0800

    first commit

---
+++ src/main/java/com/sqnu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.sqnu.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello!git!");
+        System.out.println("hello!git2!");
+        System.out.println("hello!git3!");
+        System.out.println("hello!git!hot-fix!");
+        System.out.println("master test");
+        System.out.println("hot-fix test");
+    }
+}
